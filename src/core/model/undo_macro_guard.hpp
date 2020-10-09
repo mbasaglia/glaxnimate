19505a3e5fba0f6ff1d388a6c13aa6ed428fad42
Mattia Basaglia <glax@dragon.best>
2020-10-09 19:21:35 +0200

    Add missing file

---
+++ src/core/model/undo_macro_guard.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "model/document.hpp"
+
+namespace model {
+
+
+class UndoMacroGuard
+{
+public:
+    UndoMacroGuard(const QString& name, model::Document* document, bool start_macro = true)
+    : name(name), document(document)
+    {
+        if ( start_macro )
+            start();
+    }
+
+    ~UndoMacroGuard()
+    {
+        finish();
+    }
+
+
+    void start()
+    {
+        if ( !end_macro )
+        {
+            end_macro = true;
+            document->undo_stack().beginMacro(name);
+        }
+    }
+
+    void finish()
+    {
+        if ( end_macro )
+        {
+            end_macro = false;
+            document->undo_stack().endMacro();
+        }
+    }
+
+private:
+    QString name;
+    model::Document* document;
+    bool end_macro = false;
+};
+
+
+} // namespace model
