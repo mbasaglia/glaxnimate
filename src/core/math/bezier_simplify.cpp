2afcc7b0c85f6dbaa95faac500321ab42a9b802a
Mattia Basaglia <glax@dragon.best>
2020-10-11 21:45:43 +0200

    Better curve smoothing

--- src/core/math/bezier_simplify.cpp
+++ src/core/math/bezier_simplify.cpp
@@ -15,6 +15,57 @@ qreal triangle_area(const math::Bezier& curve, int point)
     );
 }
 
+// Algoritm from https://www.particleincell.com/2012/bezier-splines/
+void auto_smooth(math::Bezier& curve)
+{
+    int n = curve.size() - 1;
+
+    // rhs vector
+    std::vector<qreal> a, b, c;
+    std::vector<QPointF> r;
+
+    // left most segment
+    a.push_back(0);
+    b.push_back(2);
+    c.push_back(1);
+    r.push_back(curve[0].pos + 2 * curve[1].pos);
+
+    // internal segments
+    for ( int i = 1; i < n - 1; i++ )
+    {
+        a.push_back(1);
+        b.push_back(4);
+        c.push_back(1);
+        r.push_back(4 * curve[i].pos + 2 * curve[i+1].pos);
+    }
+
+    // right segment
+    a.push_back(2);
+    b.push_back(7);
+    c.push_back(0);
+    r.push_back(8 * curve[n-1].pos + curve[n].pos);
+
+    // solves Ax=b with the Thomas algorithm (from Wikipedia)
+    for ( int i = 1; i < n; i++ )
+    {
+        qreal m = a[i] / b[i-1];
+        b[i] = b[i] - m * c[i - 1];
+        r[i] = r[i] - m * r[i-1];
+    }
+
+    QPointF last = r[n-1]/b[n-1];
+    curve[n-1].tan_in = last;
+    for ( int i = n - 2; i >= 0; --i )
+    {
+        last = (r[i] - c[i] * last) / b[i];
+        QPointF relative = (last - curve[i].pos);
+        curve[i].tan_in = curve[i].pos - relative;
+        curve[i].tan_out = curve[i].pos + relative;
+        curve[i].type = math::Smooth;
+    }
+
+}
+
 void math::simplify(math::Bezier& curve, qreal threshold)
 {
     if ( curve.size() < 3 || threshold <= 0 )
@@ -54,12 +105,7 @@ void math::simplify(math::Bezier& curve, qreal threshold)
             tris[index-1] = triangle_area(curve, index - 1);
     }
 
-    // Fake smoothness by making line segments curved
-    for ( int i = 1; i < curve.size() - 1; i++ )
-    {
-        curve[i].tan_in = math::lerp(curve[i].pos, curve[i-1].pos, 1/3.);
-        curve[i].tan_out = math::lerp(curve[i].pos, curve[i+1].pos, 1/3.);
-        curve[i].set_point_type(math::Smooth);
-    }
+    // Fake smoothness
+    auto_smooth(curve);
 
 }
