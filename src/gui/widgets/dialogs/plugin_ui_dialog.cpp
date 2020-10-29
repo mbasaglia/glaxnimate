7ee700723f3b0817ae51d282300c7ba5f0f8550a
Mattia Basaglia <glax@dragon.best>
2020-10-29 17:08:51 +0100

    Add Qt designer plugin

--- src/gui/widgets/dialogs/plugin_ui_dialog.cpp
+++ src/gui/widgets/dialogs/plugin_ui_dialog.cpp
@@ -3,9 +3,9 @@
 #include <QtUiTools/QUiLoader>
 #include <QVBoxLayout>
 #include <QDialogButtonBox>
-#include <QApplication>
 
 #include "app/log/log.hpp"
+#include "app/application.hpp"
 
 PluginUiDialog::PluginUiDialog(QIODevice& file, const plugin::Plugin& data, QWidget* parent)
     : QDialog(parent)
@@ -13,6 +13,9 @@ PluginUiDialog::PluginUiDialog(QIODevice& file, const plugin::Plugin& data, QWid
     QUiLoader loader;
     loader.setWorkingDirectory(data.data().dir);
     loader.setLanguageChangeEnabled(true);
+    for ( const auto& dir : app::Application::instance()->data_paths("lib") )
+        loader.addPluginPath(dir);
+
     QWidget* child = loader.load(&file, nullptr);
     if ( !child )
     {
@@ -72,6 +75,3 @@ bool PluginUiDialog::set_value(const QString& widget, const QString& property, c
 
     return wid->setProperty(property.toStdString().c_str(), value);
 }
-
-
-
