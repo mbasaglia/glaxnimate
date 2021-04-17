63219c58ad74469815bf67f1f48cab02efb402b8
Mattia Basaglia <glax@dragon.best>
2021-04-17 20:45:14 +0200

    Font description object

--- src/core/model/shapes/text.cpp
+++ src/core/model/shapes/text.cpp
@@ -1,11 +1,64 @@
 #include "text.hpp"
 
+
+GLAXNIMATE_OBJECT_IMPL(model::Font)
 GLAXNIMATE_OBJECT_IMPL(model::TextShape)
 
-QRawFont model::TextShape::font() const
+model::Font::Font(model::Document* doc)
+    : Object(doc),
+    raw_(QRawFont::fromFont(query_))
 {
-    return QRawFont::fromFont(QFont("sans", size.get()));
+    family.set(raw_.familyName());
+    style.set(raw_.styleName());
+    size.set(query_.pointSize());
+    refresh_styles();
 }
+
+void model::Font::on_font_changed()
+{
+    query_ = QFont(family.get(), size.get());
+    query_.setStyleName(style.get());
+    raw_ = QRawFont::fromFont(query_);
+}
+
+void model::Font::on_family_changed()
+{
+    refresh_styles();
+    on_font_changed();
+}
+
+void model::Font::refresh_styles()
+{
+    styles_ = QFontDatabase().styles(family.get());
+    if ( !valid_style(style.get()) && !styles_.empty() )
+        style.set(styles_[0]);
+}
+
+bool model::Font::valid_style(const QString& style)
+{
+    return styles_.contains(style);
+}
+
+const QFont & model::Font::query() const
+{
+    return query_;
+}
+
+const QRawFont & model::Font::raw_font() const
+{
+    return raw_;
+}
+
+const QStringList & model::Font::styles() const
+{
+    return styles_;
+}
+
+QString model::Font::type_name_human() const
+{
+    return tr("Font");
+}
+
 
 void model::TextShape::add_shapes(model::FrameTime t, math::bezier::MultiBezier& bez) const
 {
@@ -15,7 +68,7 @@ void model::TextShape::add_shapes(model::FrameTime t, math::bezier::MultiBezier&
 QPainterPath model::TextShape::to_painter_path(model::FrameTime t) const
 {
     QPainterPath p;
-    QRawFont font = this->font();
+    QRawFont font = this->font->raw_font();
     auto glyhs = font.glyphIndexesForString(text.get());
     auto advances = font.advancesForGlyphIndexes(glyhs, QRawFont::KernedAdvances);
     QPointF start = position.get_at(t);
@@ -37,4 +90,9 @@ QIcon model::TextShape::tree_icon() const
 QRectF model::TextShape::local_bounding_rect(model::FrameTime t) const
 {
     return to_painter_path(t).boundingRect();
+}
+
+QString model::TextShape::type_name_human() const
+{
+    return tr("Text");
 }
--- src/core/model/shapes/text.hpp
+++ src/core/model/shapes/text.hpp
@@ -1,24 +1,52 @@
 #pragma once
 
-#include "shape.hpp"
 #include <QRawFont>
 
+#include "model/property/sub_object_property.hpp"
+#include "shape.hpp"
+
 namespace model {
+
+class Font : public Object
+{
+    GLAXNIMATE_OBJECT(Font)
+    GLAXNIMATE_PROPERTY(QString, family, "", &Font::on_family_changed, {}, PropertyTraits::Visual)
+    GLAXNIMATE_PROPERTY(float, size, 32, &Font::on_font_changed, {}, PropertyTraits::Visual)
+    GLAXNIMATE_PROPERTY(QString, style, "", &Font::on_font_changed, {}, PropertyTraits::Visual)
+
+public:
+    explicit Font(Document* doc);
+
+    const QRawFont& raw_font() const;
+    const QFont& query() const;
+    const QStringList& styles() const;
+    QString type_name_human() const override;
+
+private:
+    void on_family_changed();
+    void on_font_changed();
+    bool valid_style(const QString& style);
+    void refresh_styles();
+
+    QStringList styles_;
+    QFont query_;
+    QRawFont raw_;
+};
 
 class TextShape : public ShapeElement
 {
     GLAXNIMATE_OBJECT(TextShape)
-    GLAXNIMATE_PROPERTY(QString, text, {})
+    GLAXNIMATE_PROPERTY(QString, text, {}, {}, PropertyTraits::Visual)
     GLAXNIMATE_ANIMATABLE(QPointF, position, QPointF())
-    GLAXNIMATE_PROPERTY(float, size, 32)
+    GLAXNIMATE_SUBOBJECT(Font, font)
 
 public:
     using ShapeElement::ShapeElement;
     void add_shapes(FrameTime t, math::bezier::MultiBezier& bez) const override;
     QPainterPath to_painter_path(FrameTime t) const override;
-    QRawFont font() const;
     QIcon tree_icon() const override;
     QRectF local_bounding_rect(FrameTime t) const override;
+    QString type_name_human() const override;
 };
 
 } // namespace model
