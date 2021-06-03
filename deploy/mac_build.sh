97454e21edc52f837d09c4f16f3e3f6847821756
Mattia Basaglia <glax@dragon.best>
2021-06-03 08:42:08 +0200

    Use mac_build for travis

--- deploy/mac_build.sh
+++ deploy/mac_build.sh
@@ -18,15 +18,16 @@ case "$ACTION" in
     deps)
         brew list cmake || brew install cmake
         brew list qt@5 || brew install qt@5
-        brew upgrade python@3.8 || true
+        brew upgrade python@3.9 || true
         brew list potrace || brew install potrace
         brew list ffmpeg || brew install ffmpeg
         ;;
 
     build)
+        SUFFIX="$2"
         mkdir -p "$ROOT/build"
         cd "$ROOT/build"
-        cmake .. -DQt5_DIR="$(brew --prefix qt)/lib/cmake/Qt5" -DCMAKE_PREFIX_PATH="$(brew --prefix qt)/lib/cmake/Qt5Designer"
+        cmake .. -DQt5_DIR="$(brew --prefix qt)/lib/cmake/Qt5" -DCMAKE_PREFIX_PATH="$(brew --prefix qt)/lib/cmake/Qt5Designer" -DVERSION_SUFFIX="$SUFFIX"
         make -j4
         make translations
         mkdir -p glaxnimate.iconset
@@ -40,8 +41,32 @@ case "$ACTION" in
 
     deploy)
         BRANCH="${2:-master}"
-        "$ROOT/deploy/gitlab_upload.py" "$ROOT/build/glaxnimate.dmg" "$BRANCH/MacOs/glaxnimate.dmg"
-        "$ROOT/deploy/gitlab_upload.py" "$ROOT/build/checksum.txt" "$BRANCH/MacOs/checksum.txt"
+        SSH_ARGS="$3"
+
+        if [ "$BRANCH" = macbuild -o "$BRANCH" = master ]
+        then
+            path=master
+            channel="mac-beta"
+        else
+            path="$TRAVIS_BRANCH"
+            channel="mac-stable"
+        fi
+
+        cd "$ROOT/build"
+        mkdir -p "artifacts/$path/MacOs"
+        cp glaxnimate-x86_64.zip "artifacts/$path/MacOs"
+        cp checksum.txt "artifacts/$path/MacOs"
+        cd artifacts
+
+        # Upload SourceForge
+        rsync -a "$path" mbasaglia@frs.sourceforge.net:/home/frs/project/glaxnimate/ -e "ssh -o StrictHostKeyChecking=no $SSH_ARGS"
+
+        # Upload itch.io
+        brew list wget || brew install wget
+        wget https://broth.itch.ovh/butler/darwin-amd64/LATEST/archive/default
+        unzip default
+        version="$("$ROOT/deploy/get_version.sh" "$ROOT/build/CMakeCache.txt")"
+        ./butler push "$path/MacOs/glaxnimate.dmg" "MattBas/glaxnimate:$channel" --userversion "$version"
         ;;
 
     pypi)
@@ -51,16 +76,18 @@ case "$ACTION" in
         libpath="$(echo 'from distutils.util import get_platform; import sys; print("lib.%s-%d.%d" % (get_platform(), *sys.version_info[:2]))' | python3)"
         ln -sf lib py_module/build/$libpath
         make glaxnimate_python_wheel
-        make glaxnimate_python_upload
         ;;
 
     *)
         echo " # Install dependencies"
         echo "mac_build.sh deps"
+        echo
         echo " # Compile / package"
-        echo "mac_build.sh build"
+        echo "mac_build.sh build [VERSION_SUFFIX]"
+        echo
         echo " # Add package to artifacts"
-        echo "GITLAB_ACCESS_TOKEN='' mac_build.sh deploy [branch]"
+        echo "mac_build.sh deploy [BRANCH=master [SSH_ARGS]]"
+        echo
         echo " # Build and upload python package"
         echo "mac_build.sh pypi"
         ;;
