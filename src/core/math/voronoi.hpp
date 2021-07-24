266f2cd5a194ac5ceed587c1aee6fa508529c3b3
Mattia Basaglia <glax@dragon.best>
2021-07-24 15:15:37 +0200

    Improve emoji script

--- src/core/math/voronoi.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "math/bezier/bezier.hpp"
-
-namespace glaxnimate::math {
-
-bezier::MultiBezier voronoi(std::vector<QPointF> points, const QRectF& bounds);
-
-} // namespace glaxnimate::math
