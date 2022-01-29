fb864405cfaff9c198f198f6c012f6ebde43ac63
Mattia Basaglia <glax@dragon.best>
2022-01-29 12:48:21 +0100

    pypi .2

--- deploy/pypi_version_tail.sh
+++ deploy/pypi_version_tail.sh
@@ -3,4 +3,4 @@
 # We could use something like this:
 # curl -s https://pypi.org/simple/glaxnimate/ | grep -Eo "glaxnimate-[0-9.]+" | sort | tail -n 1 | cut -d . -f 4
 # Then increase it but it might cause different CI jobs to end up with different versions
-echo .1
+echo .2
