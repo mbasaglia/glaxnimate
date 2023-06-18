ffe3b37514d48d6ab8b64fb588bc9cbd4cd8d419
Mattia Basaglia <glax@dragon.best>
2023-06-18 09:35:56 +0200

    Clean up string decoder file

--- src/core/io/aep/string_decoder.cpp
+++ src/core/io/aep/string_decoder.cpp
@@ -5,37 +5,39 @@
  */
 #include <QtGlobal>
 
+#include "io/aep/string_decoder.hpp"
+
 #if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
-#   include <QStringConverter>
-namespace glaxnimate::io::aep {
-    QString decode_string(const QByteArray& data)
-    {
-        auto encoding = QStringConverter::encodingForData(data);
-        return QStringDecoder(encoding).decode(data);
-    }
 
-    QString decode_utf16(const QByteArray& data, bool big_endian)
-    {
-        auto encoding = big_endian ? QStringConverter::Utf16BE : QStringConverter::Utf16LE;
-        return QStringDecoder(encoding).decode(data);
-    }
-} // namespace glaxnimate::io::aep
+#include <QStringConverter>
+QString glaxnimate::io::aep::decode_string(const QByteArray& data)
+{
+    auto encoding = QStringConverter::encodingForData(data);
+    return QStringDecoder(encoding).decode(data);
+}
+
+QString glaxnimate::io::aep::decode_utf16(const QByteArray& data, bool big_endian)
+{
+    auto encoding = big_endian ? QStringConverter::Utf16BE : QStringConverter::Utf16LE;
+    return QStringDecoder(encoding).decode(data);
+}
+
 #else
-#   include <QTextCodec>
-namespace glaxnimate::io::aep {
-    QString decode_string(const QByteArray& data)
-    {
-        auto utf8 = QTextCodec::codecForName("UTF-8");
-        auto encoding = QTextCodec::codecForUtfText(data, utf8);
-        return encoding->toUnicode(data);
-    }
 
-    QString decode_utf16(const QByteArray& data, bool big_endian)
-    {
-        const char* name = big_endian ? "UFT-16BE" : "UFT-16BE";
-        auto encoding = QTextCodec::codecForName(name);
-        return encoding->toUnicode(data);
-    }
-} // namespace glaxnimate::io::aep
+#include <QTextCodec>
+QString glaxnimate::io::aep::decode_string(const QByteArray& data)
+{
+    auto utf8 = QTextCodec::codecForName("UTF-8");
+    auto encoding = QTextCodec::codecForUtfText(data, utf8);
+    return encoding->toUnicode(data);
+}
+
+QString glaxnimate::io::aep::decode_utf16(const QByteArray& data, bool big_endian)
+{
+    const char* name = big_endian ? "UFT-16BE" : "UFT-16BE";
+    auto encoding = QTextCodec::codecForName(name);
+    return encoding->toUnicode(data);
+}
+
 #endif
 
