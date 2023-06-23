56808148394a556285423378ba6bb44c00d1cb2e
Mattia Basaglia <glax@dragon.best>
2023-06-23 20:37:53 +0200

    Fix comp bounding box

--- src/core/model/assets/composition.cpp
+++ src/core/model/assets/composition.cpp
@@ -48,11 +48,9 @@ int glaxnimate::model::Composition::docnode_child_index(glaxnimate::model::Docum
     return shapes.index_of(static_cast<ShapeElement*>(dn));
 }
 
-QRectF glaxnimate::model::Composition::local_bounding_rect(FrameTime t) const
+QRectF glaxnimate::model::Composition::local_bounding_rect(FrameTime) const
 {
-    if ( shapes.empty() )
-        return QRectF(QPointF(0, 0), size());
-    return shapes.bounding_rect(t);
+    return rect();
 }
 
 QImage glaxnimate::model::Composition::render_image(float time, QSize image_size, const QColor& background) const
